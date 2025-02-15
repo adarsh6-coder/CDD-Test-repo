a6ac5c3ccdb4a918bb64f8d388caabab4aa35798
CODE-BY-ADARSH <anshumanpradhan69@gmail.com>
2025-02-15 20:28:49 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,12 @@
+
+
+function red(){
+    document.getElementById("bodycolor").style.backgroundColor ="red";
+}
+function blue(){
+    document.getElementById("bodycolor").style.backgroundColor ="blue";
+}
+function green(){
+    document.getElementById("bodycolor").style.backgroundColor ="green";
+}
+
